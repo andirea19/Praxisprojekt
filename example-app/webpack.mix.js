fcf654a251d9074ecb574bca83b04032bc174ef0
Andrea Ungerer <52411390+andirea19@users.noreply.github.com>
2022-06-16 15:03:31 +0200

    update

--- example-app/webpack.mix.js
+++ example-app/webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.jsx', 'public/js')
-    .react()
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
     .alias({
         '@': 'resources/js',
