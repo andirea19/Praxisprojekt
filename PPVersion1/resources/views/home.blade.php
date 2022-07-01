8f81bf765ff0ecec4ecf80a0db7fe2b36de5252d
andirea19 <ungerer.andrea@gmail.com>
2022-07-01 14:53:29 +0200

    update

--- PPVersion1/resources/views/home.blade.php
+++ PPVersion1/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Du hast dich eingeloggt!') }}
                 </div>
             </div>
         </div>
